f144e55dd3d371c53438075b6715acf0001332f3
michaelroscero <michaelceronio2005@gmail.com>
2026-02-21 00:28:58 -0600

    Add Event model, migration, and Prisma setup

---
+++ prisma/migrations/20260221062503_add_event_model/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "Event" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "title" TEXT NOT NULL,
+    "date" TEXT NOT NULL,
+    "location" TEXT NOT NULL,
+    "description" TEXT NOT NULL
+);
